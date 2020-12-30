e9c1daf8c4e31fa1a5bae99db57f6e08c6c814e9
Jason Reed <Jreed40@illinois.edu>
2020-12-30 15:09:19 -0600

    Added support for more vibez

--- index.js
+++ index.js
@@ -2,31 +2,29 @@ const Discord = require('discord.js')
 
 const client = new Discord.Client();
 
-const token = process.env.BOT_TOKEN
+let token
+
+if (process.env.BOT_TOKEN) {
+    token = process.env.BOT_TOKEN
+} else {
+    const config = require('./config.json')
+    token = config.token
+}
 
 const prefix = '$'
 
 const ytdl = require('ytdl-core')
 
-const christ = 'https://www.youtube.com/watch?v=Ru8DQ5f5A6U'
-const rock = 'https://www.youtube.com/watch?v=-G2854Bu-LQ'
+const links = require('./ambience.json').ambience
 
 client.on('ready', () => {
-    console.log("Ho ho ho")
+    console.log("Good Vibez Only")
 })
 
-const playMusic = (currentVoice) => {
+const playMusic = (currentVoice, link) => {
     var voiceChannel = client.channels.cache.get(currentVoice);
     voiceChannel.join().then(connection => {
-        const dispatcher = connection.play(ytdl(christ));
-        dispatcher.on('finish', () => {voiceChannel.leave();});
-    }).catch(err => console.log(err));
-}
-
-const rockOut = (currentVoice) => {
-    var voiceChannel = client.channels.cache.get(currentVoice);
-    voiceChannel.join().then(connection => {
-        const dispatcher = connection.play(ytdl(rock));
+        const dispatcher = connection.play(ytdl(link));
         dispatcher.on('finish', () => {voiceChannel.leave();});
     }).catch(err => console.log(err));
 }
@@ -34,6 +32,24 @@ const rockOut = (currentVoice) => {
 const stopMusic = (currentVoice) => {
     var voiceChannel = client.channels.cache.get(currentVoice);
     voiceChannel.leave();
+}
+
+const getNames = (links) => {
+    let ret = []
+    links.forEach((item) => {
+        ret.push(item.name)
+    })
+    return ret
+}
+
+const getLink = (links, name) => {
+    let ret
+    links.forEach((item) => {
+        if (item.name === name) {
+            ret = item.link
+        }
+    })
+    return ret
 }
 
 client.on('message', async msg => {
@@ -44,14 +60,16 @@ client.on('message', async msg => {
     if (currPrefix !== prefix || msg.author.bot) {
         return;
     }
-    if (command === 'start') {
-        playMusic(currentVoice)
-    }
     if (command === 'stop') {
         stopMusic(currentVoice)
+        return
     }
-    if (command === 'rock') {
-        rockOut(currentVoice)
+    if (command === 'help') {
+        msg.reply('' + getNames(links))
+    }
+    let link = getLink(links, command)
+    if (link) {
+        playMusic(currentVoice, link)
     }
 })
 
